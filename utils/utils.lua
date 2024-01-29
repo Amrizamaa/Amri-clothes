c2ea57c10880a8096a48bc0d266a4079e853aa77
Amrizamaa <98808286+Amrizamaa@users.noreply.github.com>
2024-01-29 14:03:39 -0500

    New Progressbar
    Translate to french temporary,  add progressbar when wearing clothes add progressbar for watch and wrist

--- utils/utils.lua
+++ utils/utils.lua
@@ -108,7 +108,151 @@ AddEventHandler('sdw-progressBar:removingClothes', function(UnWearing)
             disable = {
                 car = true,
             },
+        })
+    elseif UnWearing == 'sdw_watch' then
+        local success = lib.progressBar({
+            duration = 2500,
+            label = '⌚ | Retire sa montre',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
         })  
+    elseif UnWearing == 'sdw_bracelet' then
+        local success = lib.progressBar({
+            duration = 3500,
+            label = '📿 | Removing Bracelet!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    end
+end)
+
+AddEventHandler('sdw-progressBar:WearingClothes', function(Wearing)
+    if Wearing == 'sdw_torso' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '🧥 | Wearing Shirts!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })   
+    elseif Wearing == 'sdw_pants' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '👖 | Wearing Pants!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_shoes' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '👟 | Wearing Shoes!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_mask' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '😷 | Wearing Mask!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_hat' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '🎩 | Wearing Hat!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_bag' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '🎒 | Wearing Bag!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_glasses' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '👓 | Wearing Glasses!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_vest' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '🦺 | Wearing Vest!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_ears' then
+        local success = lib.progressBar({
+            duration = 3000,
+            label = '🎧 | Wearing Ears Accessories!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })  
+    elseif Wearing == 'sdw_chain' then
+        local success = lib.progressBar({
+            duration = 2500,
+            label = '🧣 | Wearing Chain!',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })
+    elseif Wearing == 'sdw_watch' then
+        local success = lib.progressBar({
+            duration = 2500,
+            label = '⌚ | Mets sa montre',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })
+    elseif Wearing == 'sdw_wrist' then
+        local success = lib.progressBar({
+            duration = 2500,
+            label = '📿 | Enfile son bracelet',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true,
+            },
+        })
     end
 end)
 
